f3778d6c8dc4186615abb15df9fa83235be4275d
Paulo Wolfgang <paulovictor.ps.belem@gmail.com>
2024-12-27 11:19:38 -0300

    Primeiro commit

---
+++ src/main/java/br/dev/paulowolfgang/java_8/Java_8.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.dev.paulowolfgang.java_8;
+
+/**
+ *
+ * @author pvsil
+ */
+public class Java_8 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
