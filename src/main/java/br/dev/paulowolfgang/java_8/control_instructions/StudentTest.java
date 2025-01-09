c61864e7ba3309befd82470cb47a38ff7209f612
Paulo Wolfgang <paulovictor.ps.belem@gmail.com>
2025-01-09 12:07:41 -0300

    Criadas classes de estudantes

---
+++ src/main/java/br/dev/paulowolfgang/java_8/control_instructions/StudentTest.java
@@ -0,0 +1,18 @@
+package br.dev.paulowolfgang.java_8.control_instructions;
+
+/**
+ *
+ * @author Paulo Silva
+ */
+public class StudentTest {
+    
+    public static void main(String[] args) {
+        Student account01 = new Student("Jane Green", 93.5);
+        Student account02 = new Student("John Blue", 72.75);
+        
+        System.out.printf("%s's letter grade is: %s%n",
+                account01.getName(), account01.getLetterGrade());
+        System.out.printf("%s's letter grade is: %s%n",
+                account02.getName(), account02.getLetterGrade());
+    }
+}
