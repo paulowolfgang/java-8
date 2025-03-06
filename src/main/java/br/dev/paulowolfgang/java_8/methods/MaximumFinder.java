2d98e29fbc0131ef9d7b5ab28622ce7654981927
Paulo Wolfgang <paulovictor.ps.belem@gmail.com>
2025-03-06 15:05:34 -0300

    Declarando métodos com múltiplos parâmetros

---
+++ src/main/java/br/dev/paulowolfgang/java_8/methods/MaximumFinder.java
@@ -0,0 +1,39 @@
+package br.dev.paulowolfgang.java_8.methods;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Paulo Silva
+ */
+public class MaximumFinder {
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Enter three floating-point values separated by spaces: ");
+        
+        double number1 = input.nextDouble();
+        double number2 = input.nextDouble();
+        double number3 = input.nextDouble();
+        
+        double result = maximum(number1, number2, number3);
+        
+        System.out.println("Maximum is: " + result);
+    }
+    
+    public static double maximum(double x, double y, double z){
+        
+        double maximumValue = x;
+        
+        if(y > maximumValue){
+            maximumValue = y;
+        }
+        
+        if(z > maximumValue){
+            maximumValue = z;
+        }
+        
+        return maximumValue;
+    }
+}
