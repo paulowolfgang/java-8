5f640ef214df0f32cbd4f551736ccb99c0609e0c
Paulo Wolfgang <paulovictor.ps.belem@gmail.com>
2025-03-07 14:39:56 -0300

    Sobrecarga de método

---
+++ src/main/java/br/dev/paulowolfgang/java_8/methods/MethodOverload.java
@@ -0,0 +1,22 @@
+package br.dev.paulowolfgang.java_8.methods;
+
+/**
+ *
+ * @author Paulo Silva
+ */
+public class MethodOverload {
+    public static void main(String[] args) {
+        System.out.printf("Square of integer 7 is %d%n", square(7));
+        System.out.printf("Square of double 7.5 is %f%n", square(7.5));
+    }
+    
+    public static int square(int intValue){
+        System.out.printf("%nCalled square with int argument: %d%n", intValue);
+        return intValue * intValue;
+    }
+    
+    public static double square(double doubleValue){
+        System.out.printf("%nCalled square with double argument: %f%n", doubleValue);
+        return doubleValue * doubleValue;
+    }
+}
