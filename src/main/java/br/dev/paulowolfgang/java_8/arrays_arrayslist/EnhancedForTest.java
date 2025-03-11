a0baf2b2e70be6efd23452cf4ab93dceff1e5b1a
Paulo Wolfgang <paulovictor.ps.belem@gmail.com>
2025-03-11 18:40:10 -0300

    For melhorado e arrays como parâmetros

---
+++ src/main/java/br/dev/paulowolfgang/java_8/arrays_arrayslist/EnhancedForTest.java
@@ -0,0 +1,19 @@
+package br.dev.paulowolfgang.java_8.arrays_arrayslist;
+
+/**
+ *
+ * @author Paulo Silva
+ */
+public class EnhancedForTest {
+    public static void main(String[] args) {
+        
+        int[] array = { 87, 68, 94, 100, 83, 78, 85, 91, 76, 87 };
+        int total = 0;
+        
+        for (int number: array) {
+            total += number;
+        }
+        
+        System.out.printf("Total of array elements: %d%n", total);
+    }
+}
