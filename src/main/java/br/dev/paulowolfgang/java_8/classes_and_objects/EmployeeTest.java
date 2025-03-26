af3f3011ff0ae867056de265f87b358cebd35103
Paulo Wolfgang <paulovictor.ps.belem@gmail.com>
2025-03-26 16:12:31 -0300

    Composição

---
+++ src/main/java/br/dev/paulowolfgang/java_8/classes_and_objects/EmployeeTest.java
@@ -0,0 +1,17 @@
+package br.dev.paulowolfgang.java_8.classes_and_objects;
+
+/**
+ *
+ * @author Paulo Silva
+ */
+public class EmployeeTest {
+    public static void main(String[] args) {
+        
+        Date birth = new Date(7, 24, 1949);
+        Date hire = new Date(3, 12, 1988);
+        
+        Employee employee = new Employee("Bob", "Blue", birth, hire);
+        
+        System.out.println(employee);
+    }
+}
