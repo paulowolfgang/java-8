6d1d45654eed1c9592dd01052f887946ab8f50c7
Paulo Wolfgang <paulovictor.ps.belem@gmail.com>
2025-03-27 12:42:24 -0300

    Usando BigDecimal para cálculos monetários

---
+++ src/main/java/br/dev/paulowolfgang/java_8/classes_and_objects/interest/Interest.java
@@ -0,0 +1,23 @@
+package br.dev.paulowolfgang.java_8.classes_and_objects.interest;
+
+import java.math.BigDecimal;
+import java.text.NumberFormat;
+
+/**
+ *
+ * @author Paulo Silva
+ */
+public class Interest {
+    public static void main(String[] args) {
+        
+        BigDecimal principal = BigDecimal.valueOf(1000.0);
+        BigDecimal rate = BigDecimal.valueOf(0.05);
+        
+        System.out.printf("%s%20s%n", "Year", "Amount on deposit");
+        
+        for (int year = 1; year <= 10; year++) {
+            BigDecimal amount = principal.multiply(rate.add(BigDecimal.ONE).pow(year));
+            System.out.printf("%4d%20s%n", year, NumberFormat.getCurrencyInstance().format(amount));
+        }
+    }
+}
