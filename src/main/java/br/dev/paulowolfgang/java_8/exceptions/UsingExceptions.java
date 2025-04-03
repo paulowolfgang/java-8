a67505210b013fc6062fb1da0bdbd7af006fb31b
Paulo Wolfgang <paulovictor.ps.belem@gmail.com>
2025-04-03 18:01:18 -0300

    Usando finally

---
+++ src/main/java/br/dev/paulowolfgang/java_8/exceptions/UsingExceptions.java
@@ -0,0 +1,58 @@
+package br.dev.paulowolfgang.java_8.exceptions;
+
+/**
+ *
+ * @author Paulo Silva
+ */
+public class UsingExceptions {
+    public static void main(String[] args) {
+        
+        try
+        {
+            throwException();
+        }
+        catch (Exception exception)
+        {
+            System.err.println("Exception handled in main");
+        }
+        
+        doesNotThrowException();
+    }
+
+    private static void throwException() throws Exception
+    {
+        try
+        {
+            System.out.println("Method throwException");
+            throw new Exception();
+            
+        }
+        catch(Exception exception)
+        {
+            System.err.println("Exception handled in method throwException");
+            throw exception;
+            
+        }
+        finally
+        {
+          System.err.println("Finally executed in throwException");
+        }
+    }
+
+    private static void doesNotThrowException() {
+        try
+        {
+            System.out.println("Method doesNotThrowException");
+        }
+        catch(Exception exception)
+        {
+            System.err.println(exception);
+        }
+        finally
+        {
+            System.err.println("Finally executed in doesNotThrowException");
+        }
+        
+        System.out.println("End of method doesNotThrowException");
+    }
+}
