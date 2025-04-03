aff4b0463be0991e7cc0b039c51e15f8f5e921c2
Paulo Wolfgang <paulovictor.ps.belem@gmail.com>
2025-04-03 14:19:06 -0300

    Exemplo sem tratamento de exceção (divisão por 0)

---
+++ src/main/java/br/dev/paulowolfgang/java_8/exceptions/DivideByZeroNoExceptionHandling.java
@@ -0,0 +1,30 @@
+package br.dev.paulowolfgang.java_8.exceptions;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Paulo Silva
+ */
+public class DivideByZeroNoExceptionHandling {
+    
+    public static int quotient(int numerator, int denominator)
+    {
+        return numerator / denominator;
+    }
+    
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Please enter an integer numerator: ");
+        int numerator = scanner.nextInt();
+        
+        System.out.print("Please enter an integer denominator: ");
+        int denominator = scanner.nextInt();
+        
+        int result = quotient(numerator, denominator);
+        
+        System.out.printf("%nResult: %d / %d = %d%n", numerator, denominator, result);
+    }
+}
