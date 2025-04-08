7f86b9afe828e08e03fea75c9909b918371b57b9
Paulo Wolfgang <paulovictor.ps.belem@gmail.com>
2025-04-08 14:05:40 -0300

    Concatenando strings

---
+++ src/main/java/br/dev/paulowolfgang/java_8/strings/StringConcatenation.java
@@ -0,0 +1,17 @@
+package br.dev.paulowolfgang.java_8.strings;
+
+/**
+ *
+ * @author Paulo Silva
+ */
+public class StringConcatenation {
+    public static void main(String[] args) {
+        
+        String s1 = "Happy";
+        String s2 = "Birthday";
+        
+        System.out.printf("s1 = %s%ns2 = %s%n%n",s1, s2);
+        System.out.printf("Result of s1.concat(s2) = %s%n", s1.concat(s2));
+        System.out.printf("s1 after concatenation = %s%n", s1);
+    }
+}
