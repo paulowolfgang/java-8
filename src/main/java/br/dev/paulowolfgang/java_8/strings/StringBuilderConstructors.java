e655cda7169f8487a1cd546e47ce0cf8db52f6c2
Paulo Wolfgang <paulovictor.ps.belem@gmail.com>
2025-04-09 15:32:38 -0300

    String Builder

---
+++ src/main/java/br/dev/paulowolfgang/java_8/strings/StringBuilderConstructors.java
@@ -0,0 +1,19 @@
+package br.dev.paulowolfgang.java_8.strings;
+
+/**
+ *
+ * @author Paulo Silva
+ * 
+ */
+public class StringBuilderConstructors {
+    public static void main(String[] args) {
+        
+        StringBuilder buffer1 = new StringBuilder();
+        StringBuilder buffer2 = new StringBuilder(10);
+        StringBuilder buffer3 = new StringBuilder("hello");
+        
+        System.out.printf("buffer1 = \"%s\"%n", buffer1);
+        System.out.printf("buffer2 = \"%s\"%n", buffer2);
+        System.out.printf("buffer3 = \"%s\"%n", buffer3);
+    }
+}
