4f759a5b73290a860d668d8cdf2e1d51bffb5eee
Paulo Wolfgang <paulovictor.ps.belem@gmail.com>
2025-04-11 00:10:13 -0300

    Tokenização de Strings

---
+++ src/main/java/br/dev/paulowolfgang/java_8/strings/TokenTest.java
@@ -0,0 +1,22 @@
+package br.dev.paulowolfgang.java_8.strings;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Paulo Silva
+ */
+public class TokenTest {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a sentence and press Enter");
+        String sentence = scanner.nextLine();
+        
+        String[] tokens = sentence.split(" ");
+        System.out.printf("Number of elements: %d%nThe tokens are:%n", tokens.length);
+        
+        for (String token : tokens)
+            System.out.println(token);
+    }
+}
