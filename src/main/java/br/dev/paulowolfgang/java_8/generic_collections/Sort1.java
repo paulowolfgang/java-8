a671679e35b1409e98620dd26392b44cbc5a6950
Paulo Wolfgang <paulovictor.ps.belem@gmail.com>
2025-04-15 10:38:57 -0300

    Método sort

---
+++ src/main/java/br/dev/paulowolfgang/java_8/generic_collections/Sort1.java
@@ -0,0 +1,23 @@
+package br.dev.paulowolfgang.java_8.generic_collections;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ *
+ * @author Paulo Silva
+ */
+public class Sort1
+{
+    public static void main(String[] args)
+    {
+        String[] suits = {"Hearts", "Diamonds", "Clubs", "Spades"};
+        
+        List<String> list = Arrays.asList(suits);
+        System.out.printf("Unsorted array elements: %s%n", list);
+        
+        Collections.sort(list);
+        System.out.printf("Sorted array elements: %s%n", list);
+    }
+}
