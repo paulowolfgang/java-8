eb582c1ac2a038a3488dcdb182aa62427a7e8833
Paulo Wolfgang <paulovictor.ps.belem@gmail.com>
2025-04-16 12:33:17 -0300

    Mapas

---
+++ src/main/java/br/dev/paulowolfgang/java_8/generic_collections/WordTypeCount.java
@@ -0,0 +1,59 @@
+package br.dev.paulowolfgang.java_8.generic_collections;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ *
+ * @author Paulo Silva
+ */
+public class WordTypeCount
+{
+    public static void main(String[] args)
+    {
+        Map<String, Integer> myMap = new HashMap<>();
+        
+        createMap(myMap);
+        displayMap(myMap);
+    }
+    
+    private static void createMap(Map<String, Integer> map)
+    {
+        Scanner scanner = new Scanner(System.in); // cria o scanner
+        System.out.println("Enter a string:"); // solicita a entrada do usuário
+        String input = scanner.nextLine();
+
+        String[] tokens = input.split(" ");
+
+        for (String token : tokens)
+        {
+            String word = token.toLowerCase();
+
+            if (map.containsKey(word))
+            {
+                int count = map.get(word);
+                map.put(word, count + 1);
+            }
+            else
+                map.put(word, 1);
+        }
+    }
+    
+    private static void displayMap(Map<String, Integer> map)
+    {
+        Set<String> keys = map.keySet();
+
+        TreeSet<String> sortedKeys = new TreeSet<>(keys);
+        
+        System.out.printf("%nMap contains:%nKey\t\tValue%n");
+
+        for (String key : sortedKeys)
+            System.out.printf("%-10s%10s%n", key, map.get(key));
+
+        System.out.printf("%nsize: %d%nisEmpty: %b%n",
+                map.size(), map.isEmpty());
+    }
+}
