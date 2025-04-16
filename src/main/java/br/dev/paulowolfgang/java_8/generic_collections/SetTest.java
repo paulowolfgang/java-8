b7b00fef7c4c1cbf9e2554cfa5773d1d2bf96c21
Paulo Wolfgang <paulovictor.ps.belem@gmail.com>
2025-04-16 11:28:14 -0300

    Conjuntos

---
+++ src/main/java/br/dev/paulowolfgang/java_8/generic_collections/SetTest.java
@@ -0,0 +1,37 @@
+package br.dev.paulowolfgang.java_8.generic_collections;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ *
+ * @author Paulo Silva
+ */
+public class SetTest
+{
+    public static void main(String[] args)
+    {
+        String[] colors = {"red", "white", "blue", "green", "gray",
+            "orange", "tan", "white", "cyan", "peach", "gray", "orange"};
+    
+        List<String> list = Arrays.asList(colors);
+        System.out.printf("List: %s%n", list);
+        
+        printNonDuplicates(list);
+    }
+    
+    private static void printNonDuplicates(Collection<String> values)
+    {
+        Set<String> set = new HashSet<>(values);
+        
+        System.out.printf("%nNonduplicates are: ");
+        
+        for(String value : set)
+            System.out.printf("%s ", value);
+        
+        System.out.println();
+    }
+}
---
+++ src/main/java/br/dev/paulowolfgang/java_8/generic_collections/SortedSetTest.java
@@ -0,0 +1,40 @@
+package br.dev.paulowolfgang.java_8.generic_collections;
+
+import java.util.Arrays;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+/**
+ *
+ * @author Paulo Silva
+ */
+public class SortedSetTest
+{
+    public static void main(String[] args)
+    {
+        String[] colors = {"yellow", "green", "black", "tan", "grey",
+            "white", "orange", "red", "green"};
+        
+        SortedSet<String> tree = new TreeSet<>(Arrays.asList(colors));
+        
+        System.out.print("sorted set: ");
+        printSet(tree);
+        
+        System.out.print("headSet (\"orange\"): ");
+        printSet(tree.headSet("orange"));
+
+        System.out.print("tailSet (\"orange\"): ");
+        printSet(tree.tailSet("orange"));
+        
+        System.out.printf("first: %s%n", tree.first());
+        System.out.printf("last : %s%n", tree.last());
+    }
+    
+    private static void printSet(SortedSet<String> set)
+    {
+        for (String s : set)
+            System.out.printf("%s ", s);
+
+        System.out.println();
+    }
+}
