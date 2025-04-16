0043d3a710f369934d00b7d57c00124a9a8d73f0
Paulo Wolfgang <paulovictor.ps.belem@gmail.com>
2025-04-16 10:07:39 -0300

    Classe Stack do pacote java.util

---
+++ src/main/java/br/dev/paulowolfgang/java_8/generic_collections/StackTest.java
@@ -0,0 +1,56 @@
+package br.dev.paulowolfgang.java_8.generic_collections;
+
+import java.util.EmptyStackException;
+import java.util.Stack;
+
+/**
+ *
+ * @author Paulo Silva
+ */
+public class StackTest
+{
+    public static void main(String[] args)
+    {
+        Stack<Number> stack = new Stack<>();
+        
+        stack.push(12L);
+        System.out.println("Pushed 12L");
+        printStack(stack);
+        
+        stack.push(34567);
+        System.out.println("Pushed 34567");
+        printStack(stack);
+        
+        stack.push(1.0F);
+        System.out.println("Pushed 1.0F");
+        printStack(stack);
+        
+        stack.push(1234.5678);
+        System.out.println("Pushed 1234.5678");
+        printStack(stack);
+        
+        try
+        {
+            Number removedObject = null;
+            
+            while(true)
+            {
+                removedObject = stack.pop();
+                System.out.printf("Popped %s%n", removedObject);
+                printStack(stack);
+            }
+        }
+        catch(EmptyStackException emptyStackException)
+        {
+            emptyStackException.printStackTrace();
+        }
+    }
+    
+    private static void printStack(Stack<Number> stack)
+    {
+        if(stack.isEmpty())
+            System.out.printf("stack is empty%n%n");
+        else
+            System.out.printf("stack contains: %s (top)%n", stack);
+    }
+}
