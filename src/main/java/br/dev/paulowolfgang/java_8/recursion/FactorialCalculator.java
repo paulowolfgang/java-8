b2613dcbfca8f70622230caffefef1cc4ac8a2a3
Paulo Wolfgang <paulovictor.ps.belem@gmail.com>
2025-06-27 11:56:28 -0300

    Exemplo que utiliza recursão (fatoriais)

---
+++ src/main/java/br/dev/paulowolfgang/java_8/recursion/FactorialCalculator.java
@@ -0,0 +1,28 @@
+package br.dev.paulowolfgang.java_8.recursion;
+
+/**
+ *
+ * @author Paulo Silva
+ */
+public class FactorialCalculator
+{
+    public static long factorial(long number)
+    {
+        if(number <= 1)
+        {
+            return 1;
+        }
+        else
+        {
+            return number * factorial(number - 1);
+        }
+    }
+    
+    public static void main(String[] args)
+    {
+        for (int counter = 0; counter <= 5; counter++)
+        {
+            System.out.printf("%d! = %d%n", counter, factorial(counter));
+        }
+    }
+}
