a16f4172d86101ae43c82e4ed1d2ab9946ff8206
Paulo Wolfgang <paulovictor.ps.belem@gmail.com>
2025-06-30 11:15:15 -0300

    Pesquisa linear

---
+++ src/main/java/br/dev/paulowolfgang/java_8/search_sort_big_o/LinearSearchTest.java
@@ -0,0 +1,60 @@
+package br.dev.paulowolfgang.java_8.search_sort_big_o;
+
+import java.security.SecureRandom;
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Paulo Silva
+ */
+public class LinearSearchTest
+{
+    public static int linearSearch(int data[], int searchKey)
+    {
+        for (int index = 0; index < data.length; index++)
+        {
+            if(data[index] == searchKey)
+            {
+                return index;
+            }
+        }
+        
+        return -1;
+    }
+    
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        SecureRandom generator = new SecureRandom();
+        
+        int[] data = new int[10];
+        
+        for (int i = 0; i < data.length; i++)
+        {
+            data[i] = 10 + generator.nextInt(90);
+        }
+        
+        System.out.printf("%s%n%n", Arrays.toString(data));
+        
+        System.out.print("Please enter an integer value (-1 to quit): ");
+        int searchInt = input.nextInt();
+        
+        while(searchInt != -1)
+        {
+            int position = linearSearch(data, searchInt);
+            
+            if(position == -1)
+            {
+                System.out.printf("%d was not found%n%n", searchInt);
+            }
+            else
+            {
+                System.out.printf("%d was found in position %d%n%n", searchInt, position);
+            }
+            
+            System.out.print("Please enter an integer value (-1 to quit): ");
+            searchInt = input.nextInt();
+        }
+    }
+}
