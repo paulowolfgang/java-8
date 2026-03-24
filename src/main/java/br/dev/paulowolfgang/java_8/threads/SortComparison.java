c114ac14e25e8e446f944af021dd4840e217ffe2
Paulo Wolfgang <paulovictor.ps.belem@gmail.com>
2026-03-24 20:23:52 -0300

    Tempos de sort/parallelSort com a API Date/Time do Java SE 8

---
+++ src/main/java/br/dev/paulowolfgang/java_8/threads/SortComparison.java
@@ -0,0 +1,43 @@
+package br.dev.paulowolfgang.java_8.threads;
+
+import java.security.SecureRandom;
+import java.text.NumberFormat;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Arrays;
+
+/**
+ *
+ * @author Paulo Silva
+ */
+public class SortComparison
+{
+    public static void main(String[] args)
+    {
+        SecureRandom random = new SecureRandom();
+        
+        int[] array1 = random.ints(15_000_000).toArray();
+        int[] array2 = new int[array1.length];
+        
+        System.arraycopy(array1, 0, array2, 0, array1.length);
+        
+        System.out.println("Starting sort");
+        Instant sortStart = Instant.now();
+        Arrays.sort(array1);
+        Instant sortEnd = Instant.now();
+        
+        long sortTime = Duration.between(sortStart, sortEnd).toMillis();
+        System.out.printf("Total time in milliseconds: %d%n%n", sortTime);
+        
+        System.out.println("Starting parallelSort");
+        Instant parallelSortStart = Instant.now();
+        Arrays.parallelSort(array2);
+        Instant parallelSortEnd = Instant.now();
+        
+        long parallelSortTime = Duration.between(parallelSortStart, parallelSortEnd).toMillis();
+        System.out.printf("Total time in milliseconds: %d%n%n", parallelSortTime);
+        
+        String percentege = NumberFormat.getPercentInstance().format((double) sortTime / parallelSortTime);
+        System.out.printf("%nsort took %s more time than parallelSort%n", percentege);
+    }
+}
